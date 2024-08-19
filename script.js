da094a538a4de29d4d91f9f1d7141f4c01895046
OFFIAH ANTHONY1 <ebubeoffiah95@gmail.com>
2024-08-20 00:02:48 +0100

    update

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ const inputEmail = document.getElementById("email-input");
 const submitBtn = document.getElementById("submit-btn");
 const returnMessage = document.getElementById("return-message");
 
-submitBtn.addEventListener("click", displayMessage);
-
-function displayMessage(){
+submitBtn.addEventListener("click", () => {
     if(inputEmail.value === ""){
         returnMessage.innerText = "This email is invalid. Make sure it's written like example@email.com";
        returnMessage.style.color= "red";
        returnMessage.style.display="block";
+    }else if(inputEmail.value === `${inputEmail} + @gmail.com`){
+        
     }
-}
+});
